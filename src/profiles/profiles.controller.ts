9a0309f8f91faa13aba52310530473a7ad04f6d2
adams <adamskiptalam0@gmail.com>
2026-06-24 18:19:24 +0300

    POST a profile

--- src/profiles/profiles.controller.ts
+++ src/profiles/profiles.controller.ts
@@ -1,4 +1,5 @@
-import { Controller, Get, Param, Query } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Query } from '@nestjs/common';
+import { CreateProfileDto } from './dto/create-profile.dto';
 
 @Controller('profiles')
 export class ProfilesController {
@@ -11,5 +12,13 @@ export class ProfilesController {
   @Get(':id')
   findOne(@Param('id') id: string) {
     return { id };
+  }
+  // POST /profile
+  @Post()
+  create(@Body() createProfileDto: CreateProfileDto) {
+    return {
+      name: createProfileDto.name,
+      description: createProfileDto.description,
+    };
   }
 }
