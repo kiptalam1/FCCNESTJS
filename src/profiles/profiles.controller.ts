d854057024b191f737b51bf14aa271c76efbde2d
adams <adamskiptalam0@gmail.com>
2026-06-28 15:36:00 +0300

    Pipes: Transformation

--- src/profiles/profiles.controller.ts
+++ src/profiles/profiles.controller.ts
@@ -1,11 +1,13 @@
-import { Body, Controller, Delete, Get, HttpCode, HttpStatus, Param, Post, Put, Query } from '@nestjs/common';
+import { Body, Controller, Delete, Get, HttpCode, HttpStatus, Param, ParseUUIDPipe, Post, Put, Query } from '@nestjs/common';
 import { CreateProfileDto } from './dto/create-profile.dto';
 import { UpdateProfileDto } from './dto/update-profile.dto';
 import { ProfilesService } from './profiles.service';
+import type { UUID } from 'crypto';
 
 @Controller('profiles')
 export class ProfilesController {
   constructor(private profilesService: ProfilesService) { }
+
   // GET /profiles/
   @Get()
   findAll() {
@@ -14,7 +16,7 @@ export class ProfilesController {
 
   // GET /profiles/:id
   @Get(':id')
-  findOne(@Param('id') id: string) {
+  findOne(@Param('id', ParseUUIDPipe) id: UUID) {
     return this.profilesService.findOne(id);
   }
 
@@ -27,7 +29,7 @@ export class ProfilesController {
   // PUT /profile/:id
   @Put(':id')
   updateProfile(
-    @Param('id') id: string,
+    @Param('id', ParseUUIDPipe) id: UUID,
     @Body() updateProfileDto: UpdateProfileDto,
   ) {
     return this.profilesService.update(id, updateProfileDto);
@@ -36,7 +38,7 @@ export class ProfilesController {
   // DELETE /profiles/:id
   @Delete(':id')
   @HttpCode(HttpStatus.NO_CONTENT)
-  remove(@Param('id') id: string) {
+  remove(@Param('id', ParseUUIDPipe) id: UUID) {
     return this.profilesService.remove(id);
   }
 }
